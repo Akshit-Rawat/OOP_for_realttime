c746557654f8b4d1692a518628105f24c8f6276f
Akshit-Rawat <86611560+Akshit-Rawat@users.noreply.github.com>
2024-11-06 15:19:36 +0100

    Add files via upload

---
+++ check_topic_server_final.py
@@ -0,0 +1,133 @@
+import sys
+from banyan_base import BanyanBase
+import numpy as np
+import random
+
+from buffers_1 import FIFO
+from scipy.stats import expon, norm, invgauss
+from math import sqrt
+
+
+
+
+class BuffServer(BanyanBase):
+    
+    
+    def __init__(self):
+
+        super().__init__(process_name = 'BuffServer',receive_loop_idle_addition=self.datageneration_loop,loop_time = 0.1) # super function is used for inheritence from the base class which is the banyan_base
+        #loop time set to 0.1 so that data generation happens every 0.1 second
+        #two topics have been created below so that according to topic data streaming can either be stopped or start 
+        self.set_subscriber_topic('request') #initiates the data streaming
+        self.set_subscriber_topic('interrupt') # disrupts the datastreaming
+        self.set_subscriber_topic('connected') # to check whether the client is connected
+        self.server_fifo = FIFO(1) # the size of the buffer is to set to temporal window of 1, maximum samples it can hold would be equivalent to 10 as the time constant is set to 0.1
+        self.t = 0   # initialisation of both time stamps and sample value
+        self.xt = 0
+        #self.new_sample=0
+        self.topic_=False # a boolean logic implemented to start and stop the data generation loop 
+        self.topicc=[] # to check how many samples would be sent per topic call
+        
+        
+        try:   
+            self.receive_loop()  #receive loop for accepting the topic from client
+            
+        except KeyboardInterrupt:
+            self.clean_up()
+            sys.exit()
+
+    def incoming_message_processing(self, topic, payload):
+        
+        if topic=='connected': # reception of this topic ensures that the client is connected 
+            
+            
+            print("client connected")
+            self.publish_payload({'loop_time':0.5},"me too") # this notifies the client that the server is connected and ready for further processes
+
+        
+        
+        if topic=='request':        # when topic is request, the self.topic_ is set to True which initiates the if condition in the data generation looop
+            self.topic_ = True
+            self.topicc.append(topic)  # topic request accepted and saved in the topic collection list
+            print(self.topicc)
+            
+        
+         
+            cb_data = self.server_fifo.read() # collected samples from the buffer is read and made ready to be submitted as payload
+            
+            
+            print(cb_data)
+            if not cb_data:  # this is done because, as soon as the request is sent, the buffer is read empty therefore, it caused an error in the client side as it sends an empty list, which is for a cicular buffer not valid
+                return         # does not return anything but it executes and lets the function progress to next phase
+            else:
+                payload = {'data':cb_data,'time':self.t} # collected samples are now packed in to payload. "data" has both sample time stamp and sample amplitude, with it is "time" key, which will hold time stamps as well
+                self.publish_payload(payload,"plotting") # payload is published using plotting topic as the subsriber topic of client.
+                self.server_fifo.clear() # fifo buffer and topic collector list are cleared so that new samples can be added and avoid overflow/overwriting of data 
+                self.topicc.clear() 
+        
+           
+            
+            
+                
+        elif topic == 'interrupt': # when topic interrupt is accepted, then the self topic boolean turns to false, which changes the state of the idle loop and stops it from further generating more data
+            self.topic_ = False
+            print("client interrupt")
+            self.server_fifo.clear()
+            input("Press Enter to exit")
+            
+            self.clean_up()
+            sys.exit(0)
+
+        
+             
+       
+    
+    def datageneration_loop(self): # thisis the receive addition idle loop, and it serves the function of data generation
+
+        
+            if self.topic_: # when the state changes to true, whcih happens when topic reques is received by the server. the data generation loop gets initiated 
+                (self.t,self.xt) = self.generate_next_sample(current_sample=(self.t,self.xt),tau=0.1,sigma=1) # the generate next sample function generates new samples each time the idle loop runs.
+                
+                self.server_fifo.write((self.t,self.xt), current_time=self.t) # the fifo buffer writes the generated samples.
+                print(self.topic_)
+                
+            else: # this condition is met when the server receives the topic 'interrupt'. It breaks the if condition, subsequently breaking the gneration of new samples due to loop
+                return  print(self.topic_)
+                
+          # once the topic 'request' gets received, the data as much as collected over the period of 0.5s in buffer, get sent off to the client. Request acts like a trigger to send of the collected data.      
+                    
+ 
+    def generate_next_sample(self,current_sample = None, tau=.01,sigma=1): # this is the sample generator method/function
+        if current_sample is None:
+            return 0, 0        # when no inital values assigned
+        else:
+            T = invgauss(mu=tau)  # tau is the mean of inversgauss
+            dt = T.rvs() #  this function picks the random value from the distribution
+            X = norm(loc=0, scale=sigma)
+            t, xt = current_sample # this way the sample gnerator gets updated with each loop
+            t += dt   #these update the t and xt values
+            xt += X.rvs() * sqrt(dt)  
+            return t, xt            
+                    
+        
+                
+           
+
+            
+
+
+
+        
+     
+
+def buff_server():
+    BuffServer()
+
+
+if __name__ == '__main__':
+    buff_server()
+
+
+
+
+
---
+++ k_int_buff_final.py
@@ -0,0 +1,110 @@
+import sys
+import keyboard
+from python_banyan.banyan_base import BanyanBase
+import numpy as np
+import random
+
+from buffers_1 import CircBuff
+from scipy.stats import expon, norm
+
+import matplotlib.pyplot as plt
+
+plt.ion()
+class BuffClient(BanyanBase):
+    def __init__(self,refresh_time=0.5):
+
+        super().__init__(process_name = 'BuffClient',loop_time=refresh_time,receive_loop_idle_addition=self.callloop) # refresh time is set to 0.5 s
+        
+        
+        self.set_subscriber_topic('plotting') # for triggering plotting event
+        self.set_subscriber_topic('me too') # to check whether the server is connected
+        
+        # the circular buffer has a limit of 20 as it corresponds to storing 20 datapoints.
+        # as the time constant (sample period) selected is 0.1, the data generated for a time window of 1 second
+        #would be 10. the selection of number of data points to be generated is so to reflect the 
+        # amount of data to plotted by the client. To match the refresh time. the data size would be equal to 5 data points for sampel period of 0.1 seconds to avoid overflow 
+        #buffer size also reflects the time window which will be shown, that is of 2 seconds.
+        self.refresh_time= refresh_time # thisis the referesh time
+        #plus the canvas draw() would show all the 20 datapoints and to show new datapoints everytime, the flush events is used, so that the screen can be updated 
+        
+          
+        
+        self.client_circ = CircBuff(size = 20) 
+        
+        
+        self.fig_circ, self.ax_circ = plt.subplots() # this is for ploting the data stream
+        
+        self.publish_payload({'loop_time':self.refresh_time},'connected')# to ensure server that client is connected, sends this message
+        try:
+            self.receive_loop() # when initiated, the it will look for published payload and the nsend it to message processing function for further work
+            
+        except KeyboardInterrupt:
+            self.clean_up()
+            sys.exit()
+
+    def incoming_message_processing(self, topic, payload):
+        
+
+
+        if topic=='me too': # reception of this topic ensures that the server is connected and ready for requests
+            print("server connected and up and running")
+            self.publish_payload({'loop_time':self.refresh_time},'request') # the message payload for intiating the request for data
+        
+        
+        if topic == 'plotting':  # as soon as the topic plotting is received, the payload sent gets written on the circular buffer
+            
+
+             
+
+            
+            self.client_circ.write(payload["data"]) # this way the circular buffer stores the samples which  is in the form of tuple list
+                
+            print('update plot at t={}s'.format(payload["time"]))  #receives the last timestamp of every tuple list
+            t_, x_ = zip(*self.client_circ.read()) # this unzips the list and creates two tuples, when representing timestamps and other x values
+            print(self.client_circ.read())
+            self.ax_circ.clear()  # clear operation performed to clear x axis of plotting window  so that it can be updated
+                
+
+            self.ax_circ.stem(t_, x_) # stem plot
+            self.ax_circ.set_xlim([payload["time"]-2,payload["time"]]) #payloaf["time"] represents the last time stamps, therefore the updated seen would be of every 0.5 seconds on the screen, it would happen simulatanously with plots are drawn
+            self.ax_circ.set_ylim([-10, 10]) # just y limit set to 10 as sometimes x values remain low. but for a short routine, the x values would not go much high
+            self.fig_circ.canvas.draw()  # update figure
+            self.fig_circ.canvas.flush_events() # it makes the matplotlib plot the pending events and update the whole 
+
+            if payload["time"]>=10: # the data streaming limit is set till time stamp reaches 10
+                print("Plotting complete")
+                self.publish_payload({'loop_time': self.refresh_time}, 'interrupt') # this  carries out the interrupt event and triggers termination of data gneration loop
+                input("Press Enter to exit")
+            
+                self.clean_up()
+                sys.exit(0)    
+
+                
+
+        
+        if keyboard.is_pressed('l'): # another way of terminating data generation, but it will stop it desired time stamp once the if statement with condition of timestamp >=10 is commented or deleted 
+            print("Interrupt sent")
+            self.publish_payload({'loop_time': self.refresh_time}, 'interrupt')
+            input("Press Enter to exit")
+            self.clean_up()
+            sys.exit(0)
+            
+            
+        
+        
+        
+    def callloop(self):
+        self.publish_payload({'size':self.refresh_time},'request')    # this evokes the request event which triggers the reading of new samples and sending of message payload
+        
+    
+
+
+def buff_client(): #initiates the buffclient class 
+    BuffClient()
+
+
+if __name__ == '__main__':
+    buff_client()
+
+
+    
